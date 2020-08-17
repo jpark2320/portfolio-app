88b59fb6376c5723c7e56ea96c06b8d07a94cc5e
jpark2320 <jpark2320@gmail.com>
2020-08-17 16:29:02 -0400

    Implement basic API fetch with axios

--- src/pages/portfolios.js
+++ src/pages/portfolios.js
@@ -1,11 +1,28 @@
+import axios from 'axios';
 import BaseLayout from 'components/layouts/BaseLayout';
 
-const Portfolios = () => {
+const Portfolios = (props) => {
+  const { posts } = props;
   return (
     <BaseLayout>
       <h1>Portfolios</h1>
+      <ul>
+        {posts &&
+          posts.map((post, index) => {
+            return <li key={index}>{post.title}</li>;
+          })}
+      </ul>
     </BaseLayout>
   );
+};
+
+Portfolios.getInitialProps = async () => {
+  try {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    return { posts: res.data.slice(1, 10) };
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 export default Portfolios;
