87063c9aad9cfd4fc9efffded8cf616c8c05c3c1
jpark2320 <jpark2320@gmail.com>
2020-08-17 05:08:28 -0400

    Set up Project config with basic routes

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Header from 'components/shared/Header';
+
+const Index = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Index</h1>
+    </div>
+  );
+};
+
+export default Index;
---
+++ src/pages/portfolios.js
@@ -0,0 +1,5 @@
+const Portfolios = () => {
+  return <h1>Portfolios</h1>;
+};
+
+export default Portfolios;
